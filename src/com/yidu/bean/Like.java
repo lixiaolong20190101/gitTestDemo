803d1ede53c60462150af597555f1096b1d222a6
lixiaolong20190101 <2718893665@qq.com>
2020-12-24 15:30:51 +0800

    我新增的like类

---
+++ src/com/yidu/bean/Like.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @author yjm
+ * @version 1.0
+ * @description:
+ * @date 2020/12/24 15:29
+ */
+public class Like {
+    public static void main(String[] args) {
+        System.out.println("sdfg");
+    }
+}
