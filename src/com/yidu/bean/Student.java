bea89045352cd2bad542879338f90824c91bae4c
lixiaolong20190101 <2718893665@qq.com>
2020-12-24 15:10:30 +0800

    这是我的第一次提交

---
+++ src/com/yidu/bean/Student.java
@@ -0,0 +1,14 @@
+package com.yidu.bean;
+
+/**
+ * @author yjm
+ * @version 1.0
+ * @description:
+ * @date 2020/12/24 14:52
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        System.out.println("helloWorld!");
+    }
+}
